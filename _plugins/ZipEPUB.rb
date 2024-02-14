65c0d2a1c0bc926f2700d110dc86c96e8798e1c2
Bryan Fink <bryan.fink@gmail.com>
2024-02-14 11:58:12 -0600

    silence zip progress

--- _plugins/ZipEPUB.rb
+++ _plugins/ZipEPUB.rb
@@ -1,13 +1,18 @@
 require 'fileutils'
 
+# TODO: do not re-copy and rezip each epub, if not necessary.  See
+# stash "try to use mtime to prevent recopy & rezip epub" - checking
+# mtimes here doesn't work because jekyll has removed the zip before
+# we get here, so there is no mtime to check.
+
 Jekyll::Hooks.register :site, :post_write do |site|
   puts "Adding static files and zipping epubs"
   opfs = site.pages.filter { |page| page.name == 'issue.opf' }
   opfs.each { |opf|
     FileUtils.cp_r('_epub_static/.', site.dest + File.dirname(opf.dir))
     Dir.chdir("#{site.dest}#{File.dirname(opf.dir)}") {
-      system("zip -0X ../#{File.basename(File.dirname(opf.dir))}.epub mimetype")
-      system("zip -r ../#{File.basename(File.dirname(opf.dir))}.epub META-INF EPUB")
+      system("zip -0X ../#{File.basename(File.dirname(opf.dir))}.epub mimetype > /dev/null")
+      system("zip -r ../#{File.basename(File.dirname(opf.dir))}.epub META-INF EPUB > /dev/null")
     }
   }
 end
